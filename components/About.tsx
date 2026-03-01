6d0f4eb0d5a9e277ab40c122dd3bed7ae4c4fcf6
Suraj Sahu <surajsahu61771@gmail.com>
2026-03-01 16:15:39 +0530

    fixing '

--- components/About.tsx
+++ components/About.tsx
@@ -8,7 +8,7 @@ const About = () => {
         <div className="w-full">
           <h3 className='text-4xl pb-4 font-bold'>About me :</h3>
           <p className="w-full text-lg">
-            I'm a passionate frontend developer who believes great websites should be both beautiful and functional. With expertise in React, JavaScript, and modern CSS frameworks, I transform creative designs into seamless digital experiences. When I'm not crafting pixel-perfect interfaces, you'll find me exploring the latest web technologies and optimizing user journeys. I thrive on turning complex problems into elegant solutions that users love and businesses rely on.
+            I am a passionate frontend developer who believes great websites should be both beautiful and functional. With expertise in React, JavaScript, and modern CSS frameworks, I transform creative designs into seamless digital experiences. When I'm not crafting pixel-perfect interfaces, you'll find me exploring the latest web technologies and optimizing user journeys. I thrive on turning complex problems into elegant solutions that users love and businesses rely on.
           </p>
         </div>
         <div className="w-full h-screen md:h-3/4">
